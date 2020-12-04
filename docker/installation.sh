15bb8fae005ac5f857b7fb98fb5284a9b7cca12c
easec <mats.johannesson@easec.se>
2020-12-04 14:15:11 +0100

    Update installation.sh

--- docker/installation.sh
+++ docker/installation.sh
@@ -1,6 +1,6 @@
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 
-echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' | sudo tee /etc/apt/sources.list.d/docker.lis
+echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' | sudo tee /etc/apt/sources.list.d/docker.list
 
 sudo apt-get update
 
