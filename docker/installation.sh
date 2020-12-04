19c6cd277178df30541f013f4bc045c390efec78
easec <mats.johannesson@easec.se>
2020-12-04 13:57:46 +0100

    Create installation.sh

---
+++ docker/installation.sh
@@ -0,0 +1,13 @@
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' | sudo tee /etc/apt/sources.list.d/docker.lis
+
+sudo apt-get update
+
+sudo apt-get install docker-ce
+
+sudo systemctl start docker
+
+sudo systemctl enable docker
+
+sudo usermod -aG docker $USER
